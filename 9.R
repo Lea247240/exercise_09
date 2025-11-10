8a76a1877eb371a18fc5dde44ca436305bf5ae8a
Leontyna Hofmanova <247240@vut.cz>
2025-11-10 20:07:44 +0100

    Finished exercise 9

--- 9.R
+++ 9.R
@@ -44,60 +44,124 @@ S <- DNAStringSet(c('CATGC','CTAAGT', 'GCTA', 'TTCA','ATGCATC'))
 S <- c(DNAStringSet('CATGC'), DNAStringSet('CTAAGT'),DNAStringSet('GCTA'), DNAStringSet('TTCA'), DNAStringSet('ATGCATC'))
 S
 
-
+#........................................................................................
 # experiment to consensus together
 read1 <- S[1]
-read2 <- S[2]
+read2 <- S[4]
 read1 <- reverse(read1)
-read2 <- (read2)
-max_number = 0
+
 for (i in (1:width(read1))){
   cons1 <- read1[[1]][i:length(read1)]
-  cons2 <- read2[[1]][1:i]
+  cons2 <- read2[[1]][1:i:-1]
   
   if (cons1 == cons2){
+    print(cons1)
+    print(cons2)
     number <- length(cons2)
-    if (number > max_number){
-      max_number <- 0
-    }
   }
+  print(number)
+}
 
+# function iterative function (correct overlap)
+n1 <- width(read1)
+n2 <- width(read2)
+
+for (i in (1:min(n1, n2))){ #look for max consensus this is the min consensus length max
+  cons1 <- subseq(read1, start = n1 - i + 1, end = n1) #start read1
+  cons2 <- subseq(read2, start = 1, end = i)  #end read2
+  
+  if (as.character(cons1) == as.character(cons2)){
+    print(cons1)
+    print(cons2)
+    number <- i
+  }
+  print(number)
 }
 
 read1
 read2
-read2_rev <- reverse(read2)
-read2_rev
 
 read1[[1]][1] # [[]]go to seq and index []
 
 
-# matrix
+# matrix create experiment
 n <- length(S)
-matrix <- matrix(0, n, n)
+matrix_overlap <- matrix(0,n,n, dimnames = list(as.character(S), as.character(S)))
+matrix_overlap
 
-names <- c('CATGC','CTAAGT', 'GCTA', 'TTCA','ATGCATC')
-rownames(matrix) <- names
-colnames(matrix) <- names
-matrix
+# second result with properties to namec col and row
+#matrix <- matrix(0, n, n)
+#names <- c('CATGC','CTAAGT', 'GCTA', 'TTCA','ATGCATC')
+#rownames(matrix) <- names
+#colnames(matrix) <- names
+#matrix
+n
+for (row in (1:n)){
+  for (col in (1:n)){
+    matrix_overlap[row,col] <- Overlap(S[row], S[col])
+  }
+}
+matrix_overlap
+matrix_overlap[row,col] <- Overlap(S)
+diag(matrix_overlap) <- -Inf  # nebo NA
+matrix_overlap
 
-matrix[1,2] <- 1
 
-matrix[row,col] <- Overlap(S)
+#experiment with greedy
+max_overlap <- max(matrix_overlap)
+max_overlap
 
+idx <- which((matrix_overlap == max(matrix_overlap)), arr.ind = TRUE)[1, ]
+idx
+seq1 <- S[idx[1]] #row
+seq2 <- S[idx[2]] # col
 
+seq1
+seq2
+# merge this 2 sequences
+new_seq <- xscat(subseq(seq1, 1, width(seq1)-max_overlap), seq2) # seq1 without overlap # all seq2
+new_seq
+S
+S <- c(S,new_seq)
+S <- S[-c(idx[1], idx[2])]
 
-# function..............................
-Overlap <- function(S){
+ 
+
+# .......................all final function..............................
+Overlap <- function(read1, read2){
+  n1 <- width(read1)
+  n2 <- width(read2)
+  number <- 0
   
+  for (i in (1:min(n1, n2))){ #look for max consensus this is the min consensus length max
+    cons1 <- subseq(read1, start = n1 - i + 1, end = n1) #start read1
+    cons2 <- subseq(read2, start = 1, end = i)  #end read2
+    
+    if (as.character(cons1) == as.character(cons2)){
+      #print(cons1)
+      #print(cons2)
+      number <- i
+    }
+  }
+  return(number)
 }
 
+Overlap(S[1],S[5])
 
 
 OverlapMatrix <- function(S){
-  
-}
+  n <- length(S)
+  matrix_overlap <- matrix(0,n,n, dimnames = list(as.character(S), as.character(S))) #create matrix with 0
 
+  for (row in (1:n)){
+    for (col in (1:n)){
+      matrix_overlap[row,col] <- Overlap(S[row], S[col]) #get the number of consensus
+    }
+  }
+  diag(matrix_overlap) <- -Inf  # nebo NA
+  return(matrix_overlap)
+}
+OverlapMatrix(S)
 
 GreedySuperstring <- function(S){
   while (length(S)> 1){
@@ -106,11 +170,28 @@ GreedySuperstring <- function(S){
       return (S)
     }
     else {
-      ...
+      max_overlap <- max(overlapMat) # max score from 2 seq
+      idx <- which((overlapMat == max(overlapMat)), arr.ind = TRUE)[1, ] # found idx with max score in matrix
+      #get> row col
+      #      1    1
+      seq1 <- S[idx[1]] #row
+      seq2 <- S[idx[2]] # col
+      
+      # merge this 2 sequences
+      new_seq <- xscat(subseq(seq1, 1, width(seq1)-max_overlap), seq2) # seq1 without overlap # all seq2
+      new_seq
+      
+      # add new consensus
+      S <- c(S,new_seq)
+      
+      #remove old consensus
+      S <- S[-c(idx[1], idx[2])]
     }
   }
+  return(S)
 }
-  
+library(Biostrings)
+GreedySuperstring(S <- DNAStringSet(c('CATGC','CTAAGT', 'GCTA', 'TTCA','ATGCATC')))
   
   
   
